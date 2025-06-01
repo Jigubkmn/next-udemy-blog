680567fc43efddcb3fef456e293d134279286269
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-01 22:02:32 +0900

    認証設定ファイルを作成

---
+++ src/auth.ts
@@ -0,0 +1,34 @@
+// 認証の設定ファイル
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { prisma } from './lib/prisma'
+import bcryptjs from 'bcryptjs';
+
+async function getUser(email: string) {
+  return await prisma.user.findUnique({
+    where: { email: email }})
+}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email); // ユーザー取得
+            if (!user) return nul;
+            const passwordsMatch = await bcryptjs.compare(password, user.password); // パスワード比較
+            if (passwordsMatch) return user;
+            }
+            return nul;
+      },
+    }),
+  ],
+});
