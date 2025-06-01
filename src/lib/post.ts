9cafa7c6355471f87866bdd1a9c69fceab43a4e9
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-01 20:41:25 +0900

    検索ボックスをコンポーネント化対応

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -34,3 +34,35 @@ export async function getPost(id: string) {
   })
   return post
 }
+
+export async function searchPosts(search: string ) {
+  // 全角スペースを半角スペースに変換しつつスペースで分割 (空文字などを除外)
+  const decodedSearch = decodeURIComponent(search)
+  const normalizedSearch = decodedSearch.replace(/[\s ]+/g, ' ').trim()
+  const searchWords = normalizedSearch.split(' ').filter(Boolean)
+
+  const filters = searchWords.map( word => ({
+    OR : [
+      { title: { contains: word}},
+      { content: { contains: word}},
+    ]
+  }))
+
+  // 条件に合う投稿を取得
+  return await prisma.post.findMany({
+    where: {
+      And: filters
+    },
+    include: {
+      author: {
+        select: {
+          name: true
+        }
+      }
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  })
+
+}
